237633e70c0ccbd6ff617d6839ae9efa71076bae
Josh Given <josh.given@gmail.com>
2017-01-22 13:44:06 -0700

    init filter impl

--- src/presidents/PresidentDAO.java
+++ src/presidents/PresidentDAO.java
@@ -10,9 +10,51 @@ public interface PresidentDAO {
 	@SuppressWarnings("serial")
 	public static class PresidentList extends ArrayList<President> {}
 	
-	public PresidentMap getAllPresidents();
+	public PresidentMap getPresidentsMap();
 	
 	public PresidentList getFilteredPresidents(Predicate<President> filter);
 	
 	public President getPresident(int ordinal);
+	
+	public default PresidentList getAllPresidents() {
+		return getFilteredPresidents( p -> true );
+	}
+	
+	public default List<String> getParties() {
+		return getParties(getAllPresidents());
+	}
+	
+	public default List<String> getParties(PresidentList presidents) {
+		return getValueList(presidents, President::getParty );
+	}
+	
+	public default List<String> getFirstNames() {
+		return getFirstNames(getAllPresidents());
+	}
+	
+	public default List<String> getFirstNames(PresidentList presidents) {
+		return getValueList(presidents, President::getFirstName );
+	}
+
+	public default List<String> getLastNames() {
+		return getLastNames(getAllPresidents());
+	}
+
+	public default List<String> getLastNames(PresidentList presidents) {
+		return getValueList(presidents, President::getLastName );
+	}
+	
+	public default List<Integer> getTermLengths() {
+		return getTermLengths(getAllPresidents());
+	}
+	
+	public default List<Integer> getTermLengths(PresidentList presidents) {
+		return getValueList(presidents, p -> p.getEndTerm() - p.getStartTerm() );
+	}
+	
+	public default <T> List<T> getValueList(PresidentList presidents, Function<President,T> pred) {
+		List<T> values = new ArrayList<>();
+		
+		return values;
+	}
 }
--- src/web/PresidentsServlet.java
+++ src/web/PresidentsServlet.java
@@ -1,7 +1,8 @@
 package web;
 
 import java.io.IOException;
-import java.util.function.Predicate;
+import java.util.*;
+import java.util.function.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -26,8 +27,82 @@ public class PresidentsServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		PresidentList list = presidentDAO.getFilteredPresidents( p -> true );
+		// setup global presidents list
+		if(req.getServletContext().getAttribute("presidents") == null) {
+			PresidentList fullList = presidentDAO.getAllPresidents();
+			req.getServletContext().setAttribute("presidents", fullList);
+		}
+		
+		// setup filter attribute
+		String[] filterParams = req.getParameterValues("filter");
+		if(filterParams != null && filterParams.length > 0)
+		{
+			//List<Predicate<President>> filters = new ArrayList<>();
+			// Predicate<President> filter = p -> p.getParty().equals("Democrat") ;
+			// Predicate<President> filter = p -> p.getParty().equals("Republican") ;
+			Predicate<President> filter = p -> true;
+			
+			for (String param : filterParams) {
+				String[] tokens = param.split(":");
+				if(tokens.length != 3) {
+					continue;
+				}
+				
+				String item = tokens[0];
+				String op   = tokens[1];
+				String arg  = tokens[2];
+				
+				Predicate<President> pred = null;
+				
+				switch(item) {
+				case "party":
+				case "first-name":
+				case "last-name":
+					Function<President,String> presFunc = null;
+					switch(item) {
+					case "party":
+						presFunc = President::getParty;     break;
+					case "first-name":
+						presFunc = President::getFirstName; break;
+					case "last-name":
+						presFunc = President::getLastName;  break;
+					}
+					
+					BiFunction<String, String, Boolean> opFunc = null;
+					switch(op) {
+					case "equals":
+						opFunc = String::equals;     break;
+					case "contains":
+						opFunc = String::contains;   break;
+					case "starts-with":
+						opFunc = String::startsWith; break;
+					case "ends-with":
+						opFunc = String::endsWith;   break;
+					} 
+					
+					final Function<President,String>          psFunc = presFunc;
+					final BiFunction<String, String, Boolean> sbFunc = opFunc;
+					pred = (pres) -> sbFunc.apply(psFunc.apply(pres), arg);
 
+					break;
+
+				case "term-length":
+				case "term-start":
+				case "term-end":
+				default:
+				}
+				
+				if(pred != null) {
+					filter = filter.and(pred);
+				}
+			}
+			
+			PresidentList filterList = presidentDAO.getFilteredPresidents(filter);
+			req.getSession().setAttribute("presidents", filterList);
+		}
+		
+
+		
 		String mode = req.getParameter("view");
 		if(mode != null && mode.equals("pres")) {
 			try {
@@ -39,7 +114,7 @@ public class PresidentsServlet extends HttpServlet {
 				trio.setNext(presidentDAO.getPresident(ordinal+1));
 	
 				req.getSession().setAttribute("trio", trio);
-				req.getSession().setAttribute("presidents", list);
+				//req.getSession().setAttribute("presidents", list);
 				req.getRequestDispatcher("/presidents.jsp").forward(req, resp);
 				
 				return;
@@ -51,14 +126,8 @@ public class PresidentsServlet extends HttpServlet {
 			}
 		}
 		
-		
-		// Predicate<President> filter = p -> p.getParty().equals("Republican") ;
-		// Predicate<President> filter = p -> p.getParty().equals("Democrat") ;
-		Predicate<President> filter = p -> true ;
-		PresidentList filterList = presidentDAO.getFilteredPresidents(filter);
-		
-		req.getSession().setAttribute("filter", filterList);
-		req.getSession().setAttribute("presidents", list);
+		// serve index view
+		//req.getSession().setAttribute("presidents", list);
 		req.getRequestDispatcher("/index.jsp").forward(req, resp);
 	}
 	
